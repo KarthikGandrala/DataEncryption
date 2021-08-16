a02a8066e73ed7081076e5c992f1bef7ba73e1f6
Karthik Gandrala <kgandrala@gmail.com>
2021-08-17 09:47:13 +1000

    XOR documentation

--- XORCipher/XOREncrypt.py
+++ XORCipher/XOREncrypt.py
@@ -4,32 +4,58 @@
 # Function to encrypt message using key is defined
 
 def encrypt(msg, key):
+    
+    # Defining empty strings and counters
+    
     encrypt_hex = ''
     key_itr = 0
+    
+    # Running for loop in the range of MSG and comparing the BITS
+    
     for i in range(len(msg)):
         temp = ord(msg[i]) ^ ord(key[key_itr])
 
         # zfill will pad a single letter hex with 0, to make it two letter pair
 
         encrypt_hex += hex(temp)[2:].zfill(2)
+        
+        # Checking if the iterations of the key are 1
+        
         key_itr += 1
         if key_itr >= len(key):
 
             # once all of the key's letters are used, repeat the key
 
             key_itr = 0
+            
+            # Returning the final value
+            
     return encrypt_hex
 
 
 def decrypt(msg, key):
 
+    # Defining hex to uni string to store
+
     hex_to_uni = ''
+    
+    # Running for loop to the length of message
+    
     for i in range(0, len(msg), 2):
+        
+        # Decoding each individual bytes from hex
+        
         hex_to_uni += bytes.fromhex(msg[i:i + 2]).decode('utf-8')
 
     decryp_text = ''
     key_itr = 0
+    
+    # For loop running for the length of the hex to unicode string
+    
     for i in range(len(hex_to_uni)):
+        
+        # Comparing each individual bit
+        
         temp = ord(hex_to_uni[i]) ^ ord(key[key_itr])
 
         # zfill will pad a single letter hex with 0, to make it two letter pair
@@ -41,4 +67,7 @@ def decrypt(msg, key):
             # once all of the key's letters are used, repeat the key
 
             key_itr = 0
+            
+            # FInally return the decrypted text string
+            
     return decryp_text
